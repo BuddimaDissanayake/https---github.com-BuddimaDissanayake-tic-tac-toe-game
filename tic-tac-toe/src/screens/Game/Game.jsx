bf4a09272f3690af2f42670b03cdda9b83f8b4a1
BuddimaDissanayake <124259099+BuddimaDissanayake@users.noreply.github.com>
2024-03-12 11:25:57 +0530

    game functioning

--- tic-tac-toe/src/screens/Game/Game.jsx
+++ tic-tac-toe/src/screens/Game/Game.jsx
@@ -8,8 +8,7 @@ const Game = () => {
   const [grid, setGrid] = useState(Array(9).fill(null));
   const [selectedUser, setSelectedUser] = useState("");
   const [isWinner, setIsWinner] = useState(false);
-  let winner = '';
- 
+  let winner = "";
 
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
@@ -25,46 +24,48 @@ const Game = () => {
     },
   ];
 
-    const winningConditions = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-  
-    
-  const pressSquare = (index) => {
-    if (grid[index] == null) {
-      setGrid((prevState) => {
-        prevState[index] = selectedUser.id === 1 ? "o" : "x";
-        setSelectedUser(users[selectedUser.id === 1 ? 0 : 1]);
-    
-         winner = chechWinner(grid);
-
-        if (winner){
-          setIsWinner(true);
-        }
-        
-
-        return [...prevState];
-      });
-     
-    }
-  };
+  const winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
 
   const chechWinner = (grid) => {
-    for(let i = 0; i < winningConditions.length; i++){
-      const [a,b,c] = winningConditions[i];
-      if ( grid[a] === grid[b] && grid[b] === grid[c]){
+    for (let i = 0; i < winningConditions.length; i++) {
+      const [a, b, c] = winningConditions[i];
+      if (grid[a] === grid[b] && grid[b] === grid[c]) {
         console.log(grid[a]);
         return grid[a];
       }
     }
-  }
+  };
+
+  const pressSquare = (index) => {
+    
+    
+
+    
+    
+    if (grid[index] == null) {
+      setGrid((prevState) => {
+        prevState[index] = selectedUser.id === 1 ? "o" : "x";
+        setSelectedUser(users[selectedUser.id === 1 ? 0 : 1]);
+
+        winner = chechWinner(grid);
+        if (winner) {
+          setIsWinner(true);
+        }
+
+        return [...prevState];
+      });
+    }
+  };
+
 
   const changeUser = () => {
     const min = 0;
@@ -89,20 +90,23 @@ const Game = () => {
     <div className="container">
       <p className="topic">Tic Tac Toe</p>
       <Players users={users} selectedUser={selectedUser} />
-      {isWinner ? (<div className="winMessage">
-        <h1>Player { winner === 'x' ? '1' : '2'} Wins!</h1>
+      {isWinner ? (
+        <div className="winMessage">
+          {/* <h1>Player {selectedUser.name} Wins!</h1> */}
+          <h1>Good Luck!</h1>
         </div>
       ) : (
-      <div className="board">
-        {grid.map((item, index) => (
-          <Square
-            key={index.toString()}
-            index={index}
-            value={item}
-            pressSquare={() => pressSquare(index)}
-          />
-        ))}
-      </div>)}
+        <div className="board">
+          {grid.map((item, index) => (
+            <Square
+              key={index.toString()}
+              index={index}
+              value={item}
+              pressSquare={() => pressSquare(index)}
+            />
+          ))}
+        </div>
+      )}
       <button className="resetbtn" onClick={resetGame}>
         RESTART
       </button>
